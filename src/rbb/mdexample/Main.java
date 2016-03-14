0fc1694d0948633c5f42377eb808ed95f4f5addf
ReedBreneman <reedbbreneman@gmail.com>
2016-03-14 11:16:14 -0400

    Initial project setup

---
+++ src/rbb/mdexample/Main.java
@@ -0,0 +1,40 @@
+/**
+ * This is the main method. 
+ * It handles the input line parameters and reading the data into strings.
+ * The DependencyEvaluator does most of the work after the data is read in.
+ * 
+ * I use the global LOGGER throughout to log message at various levels.
+ * 
+ * @author reed
+ */
+package rbb.mdexample;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+
+	private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+	public static void main(String[] args) {
+
+		LOGGER.setLevel(Level.SEVERE);
+		
+		String[] inputData = {
+				   "A B C",
+				   "B C E",
+				   "C G",
+				   "D A F",
+				   "E F",
+				   "F H",
+				   "W"
+				};
+	
+		
+		System.out.println("Example:");
+		for (int i = 0; i<inputData.length; i++) {
+			System.out.println(inputData[i]);
+		}
+	}
+
+}
